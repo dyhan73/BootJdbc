0fb70c8a8367ef366de30d1781377ade1936920e
한대영 <dyhan@sk.com>
2019-04-01 19:35:12 +0900

    add files for Ch02/CustomerLog4Jdbc

---
+++ Ch02/CustomerLog4Jdbc/src/test/java/com/example/customerlog4jdbc/CustomerLog4jdbcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.customerlog4jdbc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerLog4jdbcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
