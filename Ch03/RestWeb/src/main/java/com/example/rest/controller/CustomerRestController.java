0c156899c70c7b1a847b994601393c118d25c5ef
한대영 <dyhan@sk.com>
2019-04-03 16:20:31 +0900

    add CRUD APIs for customer

--- Ch03/RestWeb/src/main/java/com/example/rest/controller/CustomerRestController.java
+++ Ch03/RestWeb/src/main/java/com/example/rest/controller/CustomerRestController.java
@@ -3,10 +3,8 @@ package com.example.rest.controller;
 import com.example.rest.domain.Customer;
 import com.example.rest.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,13 +16,32 @@ public class CustomerRestController {
 
     @RequestMapping(method = RequestMethod.GET)
     List<Customer> getCustomers() {
-        List<Customer> customers = customerService.findAll();
-        return customers;
+        return customerService.findAll();
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
     Customer getCustomer(@PathVariable Integer id) {
-        Customer customer = customerService.findOne(id);
-        return customer;
+        return customerService.findOne(id);
+    }
+
+    // add new customer
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    Customer postCustomers(@RequestBody Customer customer) {
+        return customerService.create(customer);
+    }
+
+    // update customer information
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    Customer putCustomer(@PathVariable Integer id, @RequestBody Customer customer) {
+        customer.setId(id);
+        return customerService.update(customer);
+    }
+
+    // delete customer
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deleteCustomer(@PathVariable Integer id) {
+        customerService.delete(id);
     }
 }
