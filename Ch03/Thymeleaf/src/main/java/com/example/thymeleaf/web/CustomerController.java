6c5c03a64ccd212218ba865a11b75d48064d63ec
한대영 <dyhan@sk.com>
2019-04-03 20:21:28 +0900

    add form to create new customer

--- Ch03/Thymeleaf/src/main/java/com/example/thymeleaf/web/CustomerController.java
+++ Ch03/Thymeleaf/src/main/java/com/example/thymeleaf/web/CustomerController.java
@@ -2,9 +2,13 @@ package com.example.thymeleaf.web;
 
 import com.example.thymeleaf.domain.Customer;
 import com.example.thymeleaf.service.CustomerService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,5 +25,22 @@ public class CustomerController {
         List<Customer> customers = customerService.findAll();
         model.addAttribute("customers", customers);
         return "customers/list";
+    }
+
+    @ModelAttribute
+    CustomerForm setUpForm() {
+        return new CustomerForm();
+    }
+
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    String create(@Validated CustomerForm form, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return list(model);
+        }
+
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(form, customer);
+        customerService.create(customer);
+        return "redirect:/customers";
     }
 }
