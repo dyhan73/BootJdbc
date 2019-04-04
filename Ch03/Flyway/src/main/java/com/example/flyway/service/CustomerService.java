d2788d740fdd28dabc238c751be607cc1b5c0769
한대영 <dyhan@sk.com>
2019-04-04 16:29:36 +0900

    init and complete Flyway example

---
+++ Ch03/Flyway/src/main/java/com/example/flyway/service/CustomerService.java
@@ -0,0 +1,36 @@
+package com.example.flyway.service;
+
+import com.example.flyway.domain.Customer;
+import com.example.flyway.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public List<Customer> findAll() {
+        return customerRepository.findAllOrderByName();
+    }
+
+    public Customer findOne(Integer id) {
+        return customerRepository.findById(id).get();
+    }
+
+    public Customer create(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer update(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public void delete(Integer id) {
+        customerRepository.deleteById(id);
+    }
+}
