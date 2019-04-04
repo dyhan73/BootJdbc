756ccccbb6612ba62c2a3bbbf154c25efa5a9f98
한대영 <dyhan@sk.com>
2019-04-04 16:47:25 +0900

    init Spring Security example

---
+++ Ch03/Security/src/main/java/com/example/security/service/CustomerService.java
@@ -0,0 +1,36 @@
+package com.example.security.service;
+
+import com.example.security.domain.Customer;
+import com.example.security.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public List<Customer> findAll() {
+        return customerRepository.findAllOrderByName();
+    }
+
+    public Customer findOne(Integer id) {
+        return customerRepository.findById(id).get();
+    }
+
+    public Customer create(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer update(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public void delete(Integer id) {
+        customerRepository.deleteById(id);
+    }
+}
---
+++ Ch03/Security/src/main/resources/db/migration/V2__import-initial-data.sql
@@ -0,0 +1,4 @@
+insert into customers(first_name, last_name) values ('대영', '한');
+insert into customers(first_name, last_name) values ('Takeshi', 'Goda');
+insert into customers(first_name, last_name) values ('Suneo', 'Honekawa');
+insert into customers(first_name, last_name) values ('Shizuka', 'Minamoto');
