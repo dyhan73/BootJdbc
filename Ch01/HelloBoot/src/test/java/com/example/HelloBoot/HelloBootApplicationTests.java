95e2c08d3784e48b78e5871cad7dee44927a65ef
한대영 <dyhan@sk.com>
2019-04-05 15:43:27 +0900

    change package names for tests

--- Ch01/HelloBoot/src/test/java/com/example/HelloBoot/HelloBootApplicationTests.java
+++ Ch01/HelloBoot/src/test/java/com/example/HelloBoot/HelloBootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.skplanet.dxp.HelloBoot;
+package com.example.HelloBoot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
--- Ch02/BootDi/src/test/java/com/example/bootdi/BootDiApplicationTests.java
+++ Ch02/BootDi/src/test/java/com/example/bootdi/BootDiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.skplanet.dxp.customer;
+package com.example.bootdi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CustomerApplicationTests {
+public class BootDiApplicationTests {
 
     @Test
     public void contextLoads() {
